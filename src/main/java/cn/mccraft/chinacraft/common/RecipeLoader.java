54f0995d87ae400a16e1219a9b9e96afefc852d6
Mouse <937279156@qq.com>
2017-02-01 17:11:59 +0800

    Added ingot

--- src/main/java/cn/mccraft/chinacraft/common/RecipeLoader.java
+++ src/main/java/cn/mccraft/chinacraft/common/RecipeLoader.java
@@ -1,8 +1,13 @@
 package cn.mccraft.chinacraft.common;
 
+import cn.mccraft.chinacraft.init.CCBlocks;
+import cn.mccraft.chinacraft.init.CCItems;
 import cn.mccraft.chinacraft.util.loader.ILoader;
 import cn.mccraft.chinacraft.util.loader.annotation.Load;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.LoaderState;
+import static net.minecraftforge.fml.common.registry.GameRegistry.*;
+
 /**
  * Created by Mouse on 2017/1/28.
  */
@@ -15,5 +20,7 @@ public class RecipeLoader implements ILoader{
 
     @Load(LoaderState.INITIALIZATION)
     public void loadSmeltingRecipe() {
+        addSmelting(CCBlocks.COPPER_ORE, new ItemStack(CCItems.COPPER_INGOT), 0.8f);
+        addSmelting(CCBlocks.TIN_ORE, new ItemStack(CCItems.TIN_INGOT), 0.8f);
     }
 }
--- src/main/java/cn/mccraft/chinacraft/init/CCItems.java
+++ src/main/java/cn/mccraft/chinacraft/init/CCItems.java
@@ -1,6 +1,6 @@
 package cn.mccraft.chinacraft.init;
 
-import cn.mccraft.chinacraft.item.ItemIngot;
+import cn.mccraft.chinacraft.item.ItemBase;
 import cn.mccraft.chinacraft.item.ItemRedPacket;
 import cn.mccraft.chinacraft.util.loader.annotation.RegItem;
 import net.minecraft.item.*;
@@ -16,8 +16,14 @@ public interface CCItems {
     @RegItem({"red", "packet"})
     ItemRedPacket RED_PACKET = new ItemRedPacket();
 
+    @RegItem(value = {"copper", "ingot"}, oreDict = {"ingotCopper"})
+    ItemBase COPPER_INGOT = new ItemBase();
+
+    @RegItem(value = {"tin", "ingot"}, oreDict = {"ingotTin"})
+    ItemBase TIN_INGOT = new ItemBase();
+
     @RegItem(value = {"bronze", "ingot"}, oreDict = {"ingotBronze"})
-    ItemIngot BRONZE_INGOT = new ItemIngot();
+    ItemBase BRONZE_INGOT = new ItemBase();
 
     /*@RegItem({"bronze", "pickaxe"})
     ItemPickaxe BRONZE_PICKAXE = new ItemPickaxe(BRONZE_TOOL_MATERIAL){};
