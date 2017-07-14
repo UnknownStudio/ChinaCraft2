fa6c2c4254b839b4912ece64eb6daef2c1ce9cda
Mouse <937279156@qq.com>
2017-07-14 16:49:59 +0800

    Add javadoc

--- src/main/java/cn/mccraft/chinacraft/util/loader/annotation/RegBlock.java
+++ src/main/java/cn/mccraft/chinacraft/util/loader/annotation/RegBlock.java
@@ -8,23 +8,37 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 注册方块的注解
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface RegBlock {
     /**
+     * 该参数将自动设置方块的registryName和unlocalizedName
      * The params to build registryName and unlocalizedName.
      * @see cn.mccraft.chinacraft.util.NameBuilder
      */
     String[] value();
 
     /**
+     * 添加矿物词典
      * All {@link net.minecraftforge.oredict.OreDictionary} values to be registered.
      */
     String[] oreDict() default {};
 
+    /**
+     * 设置方块的ItemBlock类
+     */
     Class<? extends Item> itemClass() default ItemBlock.class;
 
+    /**
+     * 是否自动注册ItemBlock
+     */
     boolean isRegisterItemBlock() default true;
 
+    /**
+     * 是否自动注册渲染器
+     */
     boolean isRegisterRender() default true;
 }
