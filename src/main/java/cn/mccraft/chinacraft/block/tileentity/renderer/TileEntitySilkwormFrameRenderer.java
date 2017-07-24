f687a0def65c3dc5f1876dcb2f7098d8122c8cc9
Lasm Gratel <lasm_gratel@hotmail.com>
2017-07-24 20:52:39 +0800

    need silkworm frame renderer

---
+++ src/main/java/cn/mccraft/chinacraft/block/tileentity/renderer/TileEntitySilkwormFrameRenderer.java
@@ -0,0 +1,10 @@
+package cn.mccraft.chinacraft.block.tileentity.renderer;
+
+import cn.mccraft.chinacraft.block.tileentity.TileEntitySilkwormFrame;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class TileEntitySilkwormFrameRenderer extends TileEntitySpecialRenderer<TileEntitySilkwormFrame> {
+}
